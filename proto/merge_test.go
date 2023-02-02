4fc4aa1a5f64d6e3c977169543942a70574c6b1e
Amaury <1293565+amaurym@users.noreply.github.com>
2023-02-02 14:53:05 +0100

    Put into another file

---
+++ proto/merge_test.go
@@ -0,0 +1,14 @@
+package proto_test
+
+import (
+	"testing"
+
+	"github.com/cosmos/gogoproto/proto"
+)
+
+func TestMergedRegistry(t *testing.T) {
+	_, err := proto.MergedRegistry()
+	if err != nil {
+		t.Error(err)
+	}
+}
